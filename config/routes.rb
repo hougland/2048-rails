755ffc49325282d8482fe13486eb813f09c67440
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-02-09 10:13:22 -0800

    added routes for API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   root "site#index"
   get "/auth/:provider/callback", to: "sessions#create"
   delete "/logout", to: 'sessions#destroy', as: :logout
+
+  # api endpoints
+  get 'games/:id' => 'games#load_game', as: :load_game
+  post 'games/:id' => 'games#save_game', as: :save_game
+  get 'users/leaderboard' => 'users#leaderboard', as: :leaderboard
+
   resources :users, :except => [:new, :create, :edit, :update]
+
 end
