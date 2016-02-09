1f81a311516057b7be454462ace33ba8857a6c03
Ricky Hougland <houglande@gmail.com>
2016-02-08 16:41:48 -0800

    create very basic leaderboard view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def index
     # leaderboard
+    @leaders = {}
+
+    User.all.each do |user|
+      @leaders[user.username] = user.best_score
+    end
+    # need to sort leaders by score
   end
 
   def show
