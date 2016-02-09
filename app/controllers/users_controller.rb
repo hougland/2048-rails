15a18c564a2c697801e46b97d0174db42930fc8d
Ricky Hougland <houglande@gmail.com>
2016-02-08 16:30:14 -0800

    add methods for crud routes that do nothing so far

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def index
+    # leaderboard
+  end
+
+  def show
+    # home page with saved games, etc.
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
